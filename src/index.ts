795260e11b98ab06d68b1495910d1209c7d9901e
Juan Lima <76479510+JELimaR@users.noreply.github.com>
2021-08-16 20:20:41 +0000

    update

--- src/index.ts
+++ src/index.ts
@@ -6,17 +6,17 @@ import VoronoiDiagram from './VoronoiDiagram';
 
 const SIZE: Vector = new Vector( {x: 3600, y: 1800} );
 
-const canvas = createCanvas(SIZE.x, SIZE.y)
+const canvas = createCanvas(SIZE.x, SIZE.y);
 const ctx = canvas.getContext('2d');
 
-let vor: VoronoiDiagram = new VoronoiDiagram(SIZE.x, SIZE.y, 15);
+let vor: VoronoiDiagram = new VoronoiDiagram(SIZE.x, SIZE.y, 1000);
 
-vor.createDiagram();
+vor.createDiagram( 0 );
 
 vor.drawDiagram( ctx );
 
-const out = fs.createWriteStream(__dirname + '/../test.png');
+const out = fs.createWriteStream( __dirname + '/../test.png');
 
 const stream = canvas.createPNGStream()
 stream.pipe(out);
-console.log('finished diagram');
+console.log('finished');
